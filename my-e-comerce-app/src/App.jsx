66d28522e8b43685e7b718d71cfae09cacc152ec
Jeffer Alexander Palomeque Caseres <81188503+alexander231987@users.noreply.github.com>
2024-02-20 22:31:23 -0500

    Inicio de APP
    En esta ocasion se inicia a programar la aplicacion iniciando por el sidebar

---
+++ my-e-comerce-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Sidebar from './components/shared/Sidebar';
+
+function App() {
+  return (
+    <div className="bg-red-400">
+      <Sidebar/>
+
+
+    </div>
+  );
+}
+
+export default App;
